0a55fada37d1c07df5b14327d4f3b8f022a3688c
Mehedi Hasan <mehedihasan2002.myc@gmail.com>
2025-08-05 03:36:57 +0600

    chore: file change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,6 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{{ config('app.name', 'Scholars Zone Global') }}</title>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
         <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
@@ -14,14 +13,15 @@
             href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css"
         />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+
 <link
   rel="stylesheet"
   href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
 />
 
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
         @stack('styles')
     </head>
     <body class="bg-gray-50">
